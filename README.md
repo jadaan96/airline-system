fd0be4237dc12e577c9a0b5441d293ad3ecf0f4f
jadaan96 <jadaan.2009@gmail.com>
2023-07-10 18:26:14 +0000

    adding uml

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # airline-system
+### UML 
+![UML](./Untitled%20(6).jpg)
--- system.js
+++ system.js
@@ -5,27 +5,32 @@ const eventsPool = require('./events');
 require('./pilot')
 require('./manager')
 let flight = {
-    event: 'new-flight',
-    time: new Date(),
-    Details: {
-      airLine: 'Royal Jordanian Airlines',
-      flightID: faker.datatype.uuid(),
-      pilot: faker.name.firstName(),
-      destination: faker.address.city(),
-    },
-  };
+  event: 'new-flight',
+  time: new Date(),
+  Details: {
+    airLine: 'Royal Jordanian Airlines',
+    flightID: faker.datatype.uuid(),
+    pilot: faker.name.firstName(),
+    destination: faker.address.city(),
+  },
+};
 
-eventsPool.on('controls' , (payload) =>{
-     setInterval(() =>{
-          payload.event = 'new-flight'
-          payload.time = new Date(),
+eventsPool.on('controls', (payload) => {
+  setInterval(() => {
+    payload.event = 'new-flight'
+    payload.time = new Date(),
 
-          eventsPool.emit('newFlight' , payload )
-          eventsPool.emit('pilot' , payload )
-     },10000)
+      eventsPool.emit('newFlight', payload)
+    setTimeout(() => {
+      eventsPool.emit('took-off', payload)
+    }, 4000)
+    setTimeout(() => {
+      eventsPool.emit('Arrived', payload)
+    }, 7000)
+  }, 10000)
 })
 
-eventsPool.emit('controls' , flight)
+eventsPool.emit('controls', flight)
 
 
 
